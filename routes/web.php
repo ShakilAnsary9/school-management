cb604d38a812bc016340b252b71d6c9ad0d8d86b
John Doe <shakilansary9@gmail.com>
2023-10-21 20:42:09 +0530

    dashboard added

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', 'App\Http\Controllers\Frontend\FrontendController@index');
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('backend.layouts.dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
